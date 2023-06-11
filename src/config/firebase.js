1de25a8549a6593a611c021fcb91c993f767bb79
shamshod1012 <122512505+shamshod1012@users.noreply.github.com>
2023-06-11 17:14:54 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
+apiKey: "AIzaSyCaS3as5UcNK0rdbCIiAxHOetfGTA-FEgY",
+  authDomain: "react-pizza-23335.firebaseapp.com",
+  projectId: "react-pizza-23335",
+  storageBucket: "react-pizza-23335.appspot.com",
+  messagingSenderId: "214263300136",
+  appId: "1:214263300136:web:b2fd5d4c3673aeb4fcf51d",
+  measurementId: "G-TPTMG3YP53",
 };
 
 const app = initializeApp(firebaseConfig);
